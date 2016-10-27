3553780baed67e1943d7b95250669bcde803f414
ArturoMondelo <arturo.mondelo@hotmail.es>
2016-10-27 23:43:31 +0200

    Update Dni.java

--- src/java/validators/Dni.java
+++ src/java/validators/Dni.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * validar matrícula
  */
 package validators;
 
@@ -14,7 +12,7 @@ import javax.validation.Payload;
 
 /**
  *
- * @author admin1
+ * @author aamondelo
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
