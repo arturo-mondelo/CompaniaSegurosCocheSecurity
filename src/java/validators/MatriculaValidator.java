3327783b8a64d081888616eee80a4d79adeecbf6
ArturoMondelo <arturo.mondelo@hotmail.es>
2016-10-27 23:42:17 +0200

    Update MatriculaValidator.java

--- src/java/validators/MatriculaValidator.java
+++ src/java/validators/MatriculaValidator.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * validar matrícula
  */
 package validators;
 
@@ -10,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 
 /**
  *
- * @author admin1
+ * @author amondelo
  */
 public class MatriculaValidator implements ConstraintValidator<Matricula,String>{
      @Override
