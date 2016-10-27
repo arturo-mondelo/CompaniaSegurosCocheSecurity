2e551297dd3419365a6e9d2cacd849088e4daa3d
ArturoMondelo <arturo.mondelo@hotmail.es>
2016-10-27 23:48:39 +0200

    comentarios

--- src/java/service/ClienteService.java
+++ src/java/service/ClienteService.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * cliente 
  */
 package service;
 
@@ -14,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
- * @author admin1
+ * @author amondelo
  */
 @Service
 @Transactional
